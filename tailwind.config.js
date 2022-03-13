cc2ca9c1ec4166d64430d676c0422436dd232967
John Mwakalinga <mwakalingajohn@gmail.com>
2022-03-13 08:56:37 +0300

    - workdone

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#a70952",
+        secondary: "#eeeff0"
+      },
+      height: {
+        120: "54rem"
+      }
+    },
+  },
+  plugins: [],
+}
+
